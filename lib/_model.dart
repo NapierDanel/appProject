661b78330e391d9f41a287cde07d40296b268f1f
NapierDanel <40511533@live.napier.ac.uk>
2021-02-23 07:48:13 +0100

    added model class not sure if needed

---
+++ lib/_model.dart
@@ -0,0 +1,12 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_mobile_app_dev/_db.dart';
+
+class Model extends ChangeNotifier {
+  DatabaseService _db = DatabaseService();
+
+  Stream<QuerySnapshot> fetchPlaniantEventsAsStream() {
+    notifyListeners();
+    return _db.streamDataCollection();
+  }
+}
