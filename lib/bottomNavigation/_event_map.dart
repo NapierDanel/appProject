ca432a2784909e16325ae2a151e34fb3958454a2
NapierDanel <40511533@live.napier.ac.uk>
2021-02-23 21:55:33 +0100

    cleanup

--- lib/bottomNavigation/_event_map.dart
+++ lib/bottomNavigation/_event_map.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/widgets.dart';
-import '_create_event_button.dart';
+import '../_create_event_button.dart';
 class _event_map extends  StatelessWidget{
   @override
   Widget build(BuildContext context) {
