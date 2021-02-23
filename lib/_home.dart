28c69be7ae18e8d02d38c03f2b536c0b91f32380
NapierDanel <40511533@live.napier.ac.uk>
2021-02-23 07:45:10 +0100

    added Stream builder to fetch firebase PlaniantEvents

--- lib/_home.dart
+++ lib/_home.dart
@@ -1,12 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
-import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_application_mobile_app_dev/_db.dart';
 
-class Home extends StatelessWidget{
+class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
-    var planiantEvents = Provider.of<List<PlaniantEvent>>(context);
-
+    DatabaseService _dbService = DatabaseService();
+    return Scaffold(
+        body: StreamBuilder<QuerySnapshot>(
+            stream: _dbService.streamDataCollection(),
+            builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              if (!snapshot.hasData) {
+                return CircularProgressIndicator();
+              }
+              return ListView(
+                children: snapshot.data.docs.map((data) {
+                  return Text(data['planiantEventBeginDate']);
+                }).toList(),
+              );
+            }));
   }
-
 }
