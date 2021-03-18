144688d5675c402862b99a0a05f5c1babb54d713
NapierDanel <40511533@live.napier.ac.uk>
2021-03-18 20:46:43 +0100

    added ListView to show first event Row

--- lib/bottomNavigation/_home.dart
+++ lib/bottomNavigation/_home.dart
@@ -9,18 +9,47 @@ class Home extends StatelessWidget {
     DatabaseService _dbService = DatabaseService();
     return Scaffold(
         body: StreamBuilder<QuerySnapshot>(
-            stream: _dbService.streamDataCollection(),
+            stream: _dbService.streamPlaniantEventsFromFirebase(),
             builder: (context, AsyncSnapshot<QuerySnapshot> snapshot) {
               if (!snapshot.hasData) {
                 return CircularProgressIndicator();
               }
-              return ListView(
-                scrollDirection: Axis.horizontal,
-                children: snapshot.data.docs.map((data)
-                {
-                  return Text(data['planiantEventDescription']);
-                }).toList(),
-              );
+              return Column(children: <Widget>[
+                SizedBox(height: 20),
+                Text('Events'),
+                SizedBox(height: 20),
+                Container(
+                  height: 150,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: snapshot.data.docs.map((data) {
+                      return Container(
+                        width: 300,
+                        height: 100,
+                        child: Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          color: Colors.amber,
+                          child: Column(
+                            mainAxisSize: MainAxisSize.min,
+                            children: <Widget>[
+                              ListTile(
+                                leading: Icon(Icons.event, size: 70), ///TODO Add leading Event Icon
+                                title: Text(data['planiantEventName'],
+                                    style: TextStyle(color: Colors.blue)),
+                                subtitle: Text(data['planiantEventDescription'],
+                                    style: TextStyle(color: Colors.blue)),
+                                //onTap: , ///TODO show Detail Event Screen
+                              ),
+                            ],
+                          ),
+                        ),
+                      );
+                    }).toList(),
+                  ),
+                ),
+              ]);
             }));
   }
 }
