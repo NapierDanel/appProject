2a6583dd928d343cbf03e82d278ec86275e39ffa
NapierDanel <40511533@live.napier.ac.uk>
2021-03-19 19:25:21 +0100

    set the current location , added event markers

--- lib/bottomNavigation/_event_map.dart
+++ lib/bottomNavigation/_event_map.dart
@@ -14,6 +14,128 @@ import 'package:map_controller/map_controller.dart';
 
 import '../_db.dart';
 
+
+class EventMap extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Lugares',
+      home: MapSample(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
+
+class MapSample extends StatefulWidget {
+  @override
+  State<MapSample> createState() => MapSampleState();
+}
+
+class MapSampleState extends State<MapSample> {
+  final FirebaseFirestore _database = FirebaseFirestore.instance;
+  Completer<GoogleMapController> _controller = Completer();
+  Map<MarkerId, Marker> markers = <MarkerId, Marker>{};
+
+  @override
+  void initState(){
+    initPlaniantEvents();
+    _currentLocation();
+    super.initState();
+  }
+
+  initPlaniantEvents(){
+    _database.collection('PlaniantEvents').get().then((docs) {
+      if(docs.docs.isNotEmpty){
+        for(int i= 0; i < docs.docs.length; i++) {
+          initMarker(docs.docs[i], docs.docs[i].id);
+        }
+      }
+    });
+  }
+  void initMarker(planiantEvent, planiantEventId) {
+    print("planiantEventId: " + planiantEventId);
+    var markerIdVal = planiantEventId;
+    final MarkerId markerId = MarkerId(markerIdVal);
+    print("TO STRING" + planiantEvent.get('planiantEventLatitude').toString());
+    final planiantEventLatitudeString =  planiantEvent.get('planiantEventLatitude').toString();
+    final planiantEventLongitudeString = planiantEvent.get('planiantEventLongitude').toString();
+
+    print(planiantEventLongitudeString + planiantEventLatitudeString);
+
+    // creating a new MARKER
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(num.parse(planiantEventLatitudeString)?.toDouble(), num.parse(planiantEventLongitudeString)?.toDouble()),
+      infoWindow: InfoWindow(title: planiantEvent.get('planiantEventName'), snippet: planiantEvent.get('planiantEventDescription')),
+    );
+
+    setState(() {
+      // adding a new marker to map
+      markers[markerId] = marker;
+    });
+  }
+
+
+  static final CameraPosition _kGooglePlex = CameraPosition(
+    target: LatLng(37.42796133580664, -122.085749655962),
+    zoom: 14.4746,
+  );
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      body: GoogleMap(
+        mapType: MapType.hybrid,
+        trafficEnabled: false,
+        buildingsEnabled: false,
+
+        initialCameraPosition: _kGooglePlex,
+        onMapCreated: (GoogleMapController controller) {
+          _controller.complete(controller);
+        },
+        myLocationEnabled: true,
+        markers: Set<Marker>.of(markers.values),
+      ),
+    );
+  }
+
+
+
+  void _currentLocation() async {
+    final GoogleMapController controller = await _controller.future;
+    LocationData currentLocation;
+    var location = new Location();
+    try {
+      currentLocation = await location.getLocation();
+    } on Exception {
+      currentLocation = null;
+    }
+
+    controller.animateCamera(CameraUpdate.newCameraPosition(
+      CameraPosition(
+        bearing: 0,
+        target: LatLng(currentLocation.latitude, currentLocation.longitude),
+        zoom: 16.0,
+      ),
+    ));
+  }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
 class EventMap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -96,13 +218,27 @@ class FireMapState extends State<FireMap> {
     _db.collection('PlaniantEvents').get().then((QuerySnapshot querySnapshot) {
       if(querySnapshot.docs.isNotEmpty){
         querySnapshot.docs.map( (event) {
-
+          for(int i = 0; i< querySnapshot.docs.length ; i++){
+            initMarker(querySnapshot.docs[i].data(), querySnapshot.docs[i].id);
+          }
         }
         );
       }
     });
   }
-/*
+  void initMarker(planiantEvent, eventId) {
+    var markerIdVal = eventId;
+    final MarkerId markerId = MarkerId(markerIdVal);
+
+    // creating a new MARKER
+    final Marker marker = Marker(
+      markerId: markerId,
+      position: LatLng(planiantEvent['Latitud'], planiantEvent['Longitud']),
+      infoWindow: InfoWindow(title: lugar['Lugar'], snippet: lugar['tipo']),
+
+    );
+  }
+
 
   _getMarkerData() async {
     FirebaseFirestore.instance.collection("PlaniantEvent").get().then(
@@ -125,4 +261,4 @@ class FireMapState extends State<FireMap> {
   }
 */
 
-}
+
