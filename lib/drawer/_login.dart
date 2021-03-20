129404c8df03c473c1af22cfbf74f7351a7f7cc4
NapierDanel <40511533@live.napier.ac.uk>
2021-03-20 14:48:39 +0100

    added firebase registration

---
+++ lib/drawer/_login.dart
@@ -0,0 +1,15 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'dart:async';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_mobile_app_dev/login/_login_page.dart';
+import '../_firebase_user.dart';
+
+class Login extends StatelessWidget{
+
+  @override
+  Widget build(BuildContext context) {
+    return LoginPage();
+  }
+
+
+}
