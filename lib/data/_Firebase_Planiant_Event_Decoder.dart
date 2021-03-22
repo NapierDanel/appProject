f2c2d8c2ddaba6aab6e800e64b692f8aef06f9f4
NapierDanel <40511533@live.napier.ac.uk>
2021-03-22 19:54:16 +0100

    added method to serialize events

--- lib/data/_Firebase_Planiant_Event_Decoder.dart
+++ lib/data/_Firebase_Planiant_Event_Decoder.dart
@@ -3,36 +3,49 @@ import 'package:flutter_application_mobile_app_dev/data/_firebase_planiant_event
 
 class FirebasePlaniantEventDecoder {
 
-  Map<String, PlaniantEvent> getFirebaseEvents() {
-    Map<String, PlaniantEvent> firebaseEventMap;
+  static Map<String, PlaniantEvent> planiantEvents;
+  final FirebaseFirestore _database = FirebaseFirestore.instance;
 
-    FirebaseFirestore.instance
-        .collection('PlaniantEvents')
-        .get()
-        .then((QuerySnapshot querySnapshot) => {
-              querySnapshot.docs.forEach((doc) {
-                firebaseEventMap.putIfAbsent(
-                    doc['id'],
-                    () => new PlaniantEvent(
-                          planiantEventDescription:
-                              doc['planiantEventDescription'].toString(),
-                          planiantEventBeginDate:
-                              doc['planiantEventBeginDate'].toString(),
-                          planiantEventEndDate:
-                              doc['planiantEventEndDate'].toString(),
-                          planiantEventImg: doc['planiantEventImg'].toString(),
-                          planiantEventLocation:
-                              doc['planiantEventLocation'].toString(),
-                          planiantEventLongitude:
-                              doc['planiantEventLongitude'].toString(),
-                          planiantEventLatitude:
-                              doc['planiantEventLatitude'].toString(),
-                          id: doc['id'].toString(),
-                        ));
-              })
-            });
 
-    return firebaseEventMap;
+  void getFirebaseEvents() {
+
+      FirebaseFirestore.instance
+          .collection('PlaniantEvents')
+          .get()
+          .then((QuerySnapshot querySnapshot) =>
+      {
+        querySnapshot.docs.forEach((doc) {
+          planiantEvents.putIfAbsent(
+              doc['id'],
+                  () =>
+              new PlaniantEvent(
+                planiantEventDescription:
+                doc['planiantEventDescription'].toString(),
+                planiantEventBeginDate:
+                doc['planiantEventBeginDate'].toString(),
+                planiantEventEndDate:
+                doc['planiantEventEndDate'].toString(),
+                planiantEventImg: doc['planiantEventImg'].toString(),
+                planiantEventLocation:
+                doc['planiantEventLocation'].toString(),
+                planiantEventLongitude:
+                doc['planiantEventLongitude'].toString(),
+                planiantEventLatitude:
+                doc['planiantEventLatitude'].toString(),
+                id: doc['id'].toString(),
+              ));
+        })
+      });
+
+      for(PlaniantEvent e in planiantEvents.values){
+        print(e);
+      }
+
+    }
+
+
+  void addPlaniantEventToPlaniantEventsMap(PlaniantEvent planiantEvent) {
+
   }
 
 /*Map<String,PlaniantEvent> getFirebaseEventsByLocationName(String locationName){
