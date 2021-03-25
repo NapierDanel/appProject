51a75ab2114691a83d3210024b322de65128dd84
NapierDanel <40511533@live.napier.ac.uk>
2021-03-26 00:04:25 +0100

    added image Firebase Storage

--- lib/_planiant_Event_Detail_Screen.dart
+++ lib/_planiant_Event_Detail_Screen.dart
@@ -1,11 +1,15 @@
+import 'package:firebase_image/firebase_image.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_mobile_app_dev/data/_firebase_planiant_event.dart';
 import 'package:add_2_calendar/add_2_calendar.dart';
 import 'package:intl/intl.dart';
+import 'package:photo_view/photo_view.dart';
 
 class PlaniantEventDetailScreen extends StatelessWidget {
   PlaniantEvent planiantEvent;
+  Image planiantEventImage;
 
   PlaniantEventDetailScreen({Key key, @required this.planiantEvent})
       : super(key: key);
@@ -14,12 +18,37 @@ class PlaniantEventDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return ListView(children: [
       /// Image Section
-      Image.asset(
-        'assets/images/drawerHeaderImg.jpeg',
-        width: 600,
-        height: 240,
-        fit: BoxFit.cover,
-      ),
+      FutureBuilder<String>(
+          future: _getStoragePlaniantEventImageURL(planiantEvent.id.toString()),
+          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+            if (snapshot.hasData) {
+              this.planiantEventImage = Image.network(
+                snapshot.data,
+                fit: BoxFit.cover,
+                height: 300,
+                semanticLabel: 'Event Image',
+              );
+              return GestureDetector(
+                child: Hero(
+                  tag: 'imageHero',
+                  child: planiantEventImage,
+                ),
+                onTap: () {
+                  Navigator.push(context, MaterialPageRoute(builder: (_) {
+                    return DetailImageScreen(planiantEventImage);
+                  }));
+                },
+              );
+            } else if (snapshot.hasError) {
+              return Text('Failure Loading Even Image');
+            } else {
+              return SizedBox(
+                child: CircularProgressIndicator(),
+                width: 60,
+                height: 60,
+              );
+            }
+          }),
 
       /// Title Section
       Container(
@@ -87,24 +116,28 @@ class PlaniantEventDetailScreen extends StatelessWidget {
     ]);
   }
 
-  Column _choiceButtonColumn(Color color, IconData icon, String label) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        Icon(icon, color: color),
-        Container(
-          margin: const EdgeInsets.only(top: 8),
-          child: Text(
-            label,
-            style: TextStyle(
-              fontSize: 12,
-              fontWeight: FontWeight.w400,
-              color: color,
+  Container _choiceButtonColumn(Color color, IconData icon, String label) {
+    return Container(
+      color: color,
+      margin: const EdgeInsets.all(10.0),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Icon(icon, color: color),
+          Container(
+            margin: const EdgeInsets.only(top: 8),
+            child: Text(
+              label,
+              style: TextStyle(
+                fontSize: 12,
+                fontWeight: FontWeight.w400,
+                color: color,
+              ),
             ),
           ),
-        ),
-      ],
+        ],
+      ),
     );
   }
 
@@ -121,5 +154,43 @@ class PlaniantEventDetailScreen extends StatelessWidget {
     );
 
     Add2Calendar.addEvent2Cal(calendarEvent, androidNoUI: false);
+  }
+
+  Future<String> _getStoragePlaniantEventImageURL(String id) async {
+    print('in methond');
+
+    String planiantEventImagePath = 'PlaniantEventImages/' + id + '_titleImg';
+    print('planiantEventImagePath:' + planiantEventImagePath);
+
+    final ref = FirebaseStorage.instance.ref().child(planiantEventImagePath);
+
+    var url = await ref.getDownloadURL();
+    print('URL:' + url);
+    return url;
+  }
+}
+
+class DetailImageScreen extends StatelessWidget {
+  Image image;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: GestureDetector(
+        child: Center(
+          child: Hero(
+            tag: 'imageHero',
+            child: image,
+          ),
+        ),
+        onTap: () {
+          Navigator.pop(context);
+        },
+      ),
+    );
+  }
+
+  DetailImageScreen(Image image) {
+    this.image = image;
   }
 }
