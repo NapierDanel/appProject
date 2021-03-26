63b5418f0126133374e0311d34c49113ae109b4f
NapierDanel <40511533@live.napier.ac.uk>
2021-03-26 23:37:43 +0100

    changed visual

--- lib/_planiant_Event_Detail_Screen.dart
+++ lib/_planiant_Event_Detail_Screen.dart
@@ -4,122 +4,132 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_application_mobile_app_dev/data/_firebase_planiant_event.dart';
 import 'package:add_2_calendar/add_2_calendar.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:intl/intl.dart';
 import 'package:photo_view/photo_view.dart';
 
 class PlaniantEventDetailScreen extends StatelessWidget {
   PlaniantEvent planiantEvent;
   Image planiantEventImage;
+  final String noImageSvgPath = 'assets/images/no-photos.png';
 
   PlaniantEventDetailScreen({Key key, @required this.planiantEvent})
       : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return ListView(children: [
-      /// Image Section
-      FutureBuilder<String>(
-          future: _getStoragePlaniantEventImageURL(planiantEvent.id.toString()),
-          builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-            if (!snapshot.hasData) {
-              return SizedBox(
-                child: LinearProgressIndicator(),
-                width: 60,
-                height: 60,
-              );
-            } else if (snapshot.hasData) {
-              this.planiantEventImage = Image.network(
-                snapshot.data,
-                fit: BoxFit.cover,
-                height: 300,
-                semanticLabel: 'Event Image',
-              );
-              return GestureDetector(
-                child: Hero(
-                  tag: 'imageHero',
-                  child: planiantEventImage,
-                ),
-                onTap: () {
-                  Navigator.push(context, MaterialPageRoute(builder: (_) {
-                    return DetailImageScreen(planiantEventImage);
-                  }));
-                },
-              );
-            } else if (snapshot.hasError) {
-              return Text('Failure Loading Even Image');
-            } else {
-              return SizedBox(
-                child: CircularProgressIndicator(),
-                width: 60,
-                height: 60,
-              );
-            }
-          }),
+    return Scaffold(
+      body: ListView(children: [
+        /// Image Section
+        FutureBuilder<String>(
+            future:
+                _getStoragePlaniantEventImageURL(planiantEvent.id.toString()),
+            builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
+              if (snapshot.hasData) {
+                this.planiantEventImage = Image.network(
+                  snapshot.data,
+                  fit: BoxFit.cover,
+                  height: 300,
+                  semanticLabel: 'Event Image',
+                );
+                return GestureDetector(
+                  child: Hero(
+                    tag: 'imageHero',
+                    child: planiantEventImage,
+                  ),
+                  onTap: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (_) {
+                      return DetailImageScreen(planiantEventImage);
+                    }));
+                  },
+                );
+              } else if (!snapshot.hasData) {
+                return SizedBox(
+                    child: Image.asset(
+                  noImageSvgPath,
+                  height: 200,
+                  width: 300,
+                      color: Colors.orange,
+                      scale: 3,
+                ));
+              } else if (snapshot.hasError) {
+                return Text('Failure Loading Even Image');
+              } else {
+                return SizedBox(
+                    child: Image.asset(
+                  noImageSvgPath,
+                  height: 200,
+                  width: 300,
+                ));
+              }
+            }),
 
-      /// Title Section
-      Container(
-        padding: const EdgeInsets.all(32),
-        child: Row(
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Container(
-                    padding: const EdgeInsets.only(bottom: 8),
-                    child: Text(
-                      planiantEvent.planiantEventName,
+        /// Title Section
+        Container(
+          padding: const EdgeInsets.all(32),
+          child: Row(
+            children: [
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(bottom: 8),
+                      child: Text(
+                        planiantEvent.planiantEventName,
+                        style: TextStyle(
+                          fontSize: 12,
+                        ),
+                      ),
+                    ),
+                    Text(
+                      planiantEvent.planiantEventLocation,
                       style: TextStyle(
+                        color: Colors.grey[500],
                         fontSize: 12,
                       ),
                     ),
-                  ),
-                  Text(
-                    planiantEvent.planiantEventLocation,
-                    style: TextStyle(
-                      color: Colors.grey[500],
-                      fontSize: 12,
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
 
-            /// When the user  Icon Button pressed
-            IconButton(
-              onPressed: () => {
-                addPlaniantEventToCalendar(planiantEvent),
-                SnackBar(content: Text('Event added to calendar')),
-              },
-              icon: new Icon(Icons.event),
-              color: Colors.blue,
-              splashColor: Colors.orange,
-              highlightColor: Colors.blue,
-            ),
-          ],
+              /// When the user  Icon Button pressed
+              IconButton(
+                onPressed: () => {
+                  addPlaniantEventToCalendar(planiantEvent),
+                  SnackBar(content: Text('Event added to calendar')),
+                },
+                icon: new Icon(Icons.event),
+                color: Colors.blue,
+                splashColor: Colors.orange,
+                highlightColor: Colors.blue,
+              ),
+            ],
+          ),
         ),
-      ),
 
-      /// Description
-      Container(
-        padding: const EdgeInsets.all(32),
-        child: Text(planiantEvent.planiantEventDescription,
-            style: TextStyle(
-              fontSize: 12,
-            )),
-      ),
-
-      /// Choice Section
-      Container(
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-          children: [
-            _choiceButtonColumn(Colors.lightGreenAccent, Icons.add, 'I AM IN'),
-            _choiceButtonColumn(Colors.redAccent, Icons.remove, 'I AM OUT'),
-          ],
+        /// Description
+        Container(
+          padding: const EdgeInsets.all(32),
+          child: Text(planiantEvent.planiantEventDescription,
+              style: TextStyle(
+                fontSize: 12,
+              )),
         ),
-      ),
-    ]);
+
+        /// Choice Section
+        Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              _choiceButtonColumn(
+                  Colors.lightGreenAccent, Icons.add, 'I AM IN'),
+              _choiceButtonColumn(Colors.redAccent, Icons.remove, 'I AM OUT'),
+            ],
+          ),
+        ),
+      ]),
+    );
   }
 
   Container _choiceButtonColumn(Color color, IconData icon, String label) {
@@ -130,14 +140,12 @@ class PlaniantEventDetailScreen extends StatelessWidget {
         mainAxisSize: MainAxisSize.min,
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Icon(icon, color: color),
+          Icon(icon, color: Colors.orange),
           Container(
-            margin: const EdgeInsets.only(top: 8),
             child: Text(
               label,
               style: TextStyle(
                 fontSize: 12,
-                fontWeight: FontWeight.w400,
                 color: color,
               ),
             ),
@@ -163,10 +171,7 @@ class PlaniantEventDetailScreen extends StatelessWidget {
   }
 
   Future<String> _getStoragePlaniantEventImageURL(String id) async {
-    print('in methond');
-
     String planiantEventImagePath = 'PlaniantEventImages/' + id + '_titleImg';
-    print('planiantEventImagePath:' + planiantEventImagePath);
 
     final ref = FirebaseStorage.instance.ref().child(planiantEventImagePath);
 
@@ -182,6 +187,7 @@ class DetailImageScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: GestureDetector(
         child: Center(
             child: Hero(
