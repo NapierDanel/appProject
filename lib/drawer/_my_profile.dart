a90bfd3c4dfc4129c86d2352fe0ad867cc13a01a
NapierDanel <40511533@live.napier.ac.uk>
2021-03-27 00:48:32 +0100

    Test Build current User display in MyProfile

--- lib/drawer/_my_profile.dart
+++ lib/drawer/_my_profile.dart
@@ -1,11 +1,43 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyProfile extends StatelessWidget{
+class MyProfilePage extends StatefulWidget {
+  @override
+  MyProfileStage createState() => MyProfileStage();
+}
+
+
+
+class MyProfileStage extends State<MyProfilePage> {
+
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  FirebaseUser user;
+
+  @override
+  void initState() {
+    super.initState();
+    initUser();
+  }
+
+  initUser() async {
+    user = await _auth.currentUser;
+    setState(() {});
+  }
+
 
   @override
   Widget build(BuildContext context) {
+
+    if (_auth.currentUser != null) {
+      print(_auth.currentUser.uid);
+
+      return Text("Moin:" + FirebaseAuth.instance.currentUser.email);
+
+    }
+
     return Text("Hallo");
   }
-
 }
