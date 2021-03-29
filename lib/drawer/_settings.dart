bff8825b4cb5c71b13d22991f3027a4003d33a98
NapierDanel <40511533@live.napier.ac.uk>
2021-03-29 17:54:32 +0200

    added settings dep

--- lib/drawer/_settings.dart
+++ lib/drawer/_settings.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
 
 class Settings extends StatelessWidget{
 
   @override
   Widget build(BuildContext context) {
-      return Text("Hallo");
+     return  SettingsList(
+        sections: [
+          SettingsSection(
+            tiles: [
+              SettingsTile(
+                title: 'Language',
+                subtitle: 'English',
+                leading: Icon(Icons.language),
+                onPressed: (BuildContext context) {},
+              ),
+             SettingsTile(title: 'Darkmode',
+             leading: Icon(Icons.wb_sunny))
+            ],
+          ),
+        ],
+      );
   }
 
 
