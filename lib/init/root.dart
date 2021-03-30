1f693c364935474646b0142dafd011a3eee0aa78
NapierDanel <40511533@live.napier.ac.uk>
2021-03-30 13:47:49 +0200

    cleanup

--- lib/init/root.dart
+++ lib/init/root.dart
@@ -2,8 +2,6 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter_application_mobile_app_dev/init/my_app.dart';
 import 'package:provider/provider.dart';
 import '../data/_db.dart';
-import '../bottomNavigation/_home.dart';
-
 import 'package:flutter_application_mobile_app_dev/data/_firebase_planiant_event.dart';
 
 class Root extends StatelessWidget{
@@ -12,7 +10,8 @@ class Root extends StatelessWidget{
 
     final DatabaseService databaseService = DatabaseService();
 
-    return StreamProvider<List<PlaniantEvent>>.value(value: databaseService.planiantEvents, child: MyApp());
+    return StreamProvider<List<PlaniantEvent>>.value(value: databaseService.planiantEvents, initialData: [],
+    child: MyApp());
   }
 
 }
