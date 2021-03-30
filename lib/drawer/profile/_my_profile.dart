af0c25aa464b9dae4697c138ee1934d6df1dbad9
NapierDanel <40511533@live.napier.ac.uk>
2021-03-30 22:53:46 +0200

    added favorite Events to myProfile

--- lib/drawer/profile/_my_profile.dart
+++ lib/drawer/profile/_my_profile.dart
@@ -1,11 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter_application_mobile_app_dev/bottomNavigation/_planiant_Event_Detail_Screen.dart';
+import 'package:flutter_application_mobile_app_dev/data/_firebase_planiant_event.dart';
+import 'package:flutter_application_mobile_app_dev/data/_user.dart';
 import 'package:flutter_application_mobile_app_dev/drawer/profile/Avatar.dart';
 import 'package:flutter_application_mobile_app_dev/login/_login_page.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
 import 'package:firebase_storage/firebase_storage.dart' as firebase_storage;
+import 'package:provider/provider.dart';
 
 class MyProfilePage extends StatefulWidget {
   @override
@@ -18,6 +22,7 @@ class MyProfileStage extends State<MyProfilePage> {
     super.initState();
   }
 
+
   final picker = ImagePicker();
   File _image;
   User _currentUser;
@@ -28,6 +33,11 @@ class MyProfileStage extends State<MyProfilePage> {
 
   @override
   Widget build(BuildContext context) {
+
+    /// Firebase PlaniantEvents
+    List<PlaniantEvent> planiantEvents =
+    Provider.of<List<PlaniantEvent>>(context);
+
     final node = FocusScope.of(context);
 
     /// Check if the User is logged in, if not -> push LoginPage
@@ -65,11 +75,7 @@ class MyProfileStage extends State<MyProfilePage> {
                   ),
                   Text(
                     _currentUser?.displayName ?? 'No User name set',
-                    style: TextStyle(
-                      fontSize: 30,
-                      color: Colors.white
-
-                    ),
+                    style: TextStyle(fontSize: 30, color: Colors.white),
                   ),
                 ],
               ),
@@ -98,30 +104,37 @@ class MyProfileStage extends State<MyProfilePage> {
                       ),
                     ),
                     SizedBox(height: 20.0),
-                    Expanded(
-                      child: Column(
-                        children: <Widget>[
-                          TextFormField(
-                            autofocus: false,
-                            obscureText: true,
-                            controller: passwordController,
-                            textInputAction: TextInputAction.done,
-                            onFieldSubmitted: (v) {
-                              FocusScope.of(context).requestFocus(node);
-                            },
-                            decoration: InputDecoration(
-                              hintText: "new Password",
-                              contentPadding:
-                                  EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
-                              border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(32.0)),
-                            ),
+                    Column(
+                      children: <Widget>[
+                        TextFormField(
+                          autofocus: false,
+                          obscureText: true,
+                          controller: passwordController,
+                          textInputAction: TextInputAction.done,
+                          onFieldSubmitted: (v) {
+                            FocusScope.of(context).requestFocus(node);
+                          },
+                          decoration: InputDecoration(
+                            hintText: "new Password",
+                            contentPadding:
+                                EdgeInsets.fromLTRB(20.0, 10.0, 20.0, 10.0),
+                            border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(32.0)),
                           ),
-                        ],
-                      ),
+                        ),
+                      ],
                     ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text('Your Favorite Events❤️'),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    buildListView(planiantEvents),
                     Padding(
                       padding: EdgeInsets.symmetric(vertical: 10.0),
+
                       // ignore: deprecated_member_use
                       child: RaisedButton(
                         shape: RoundedRectangleBorder(
@@ -143,6 +156,40 @@ class MyProfileStage extends State<MyProfilePage> {
         ],
       ),
     );
+  }
+
+  Widget buildListView(planiantEvents) {
+    print(PlaniantUser.getPlaniantUserInstance().acceptedPlaniantEvents);
+
+    List<PlaniantEvent> favEvents = [];
+
+    for(PlaniantEvent planiantEvent in planiantEvents){
+      if(PlaniantUser.getPlaniantUserInstance().acceptedPlaniantEvents.contains(planiantEvent.id)){
+        favEvents.add(planiantEvent);
+      }
+    }
+
+    return Container(
+        width: MediaQuery.of(context).size.width,
+        height: 200,
+        color: Colors.blue,
+        child: ListView.builder(
+          itemCount: favEvents.length,
+          itemBuilder: (context, index) {
+            return Card(
+                child: ListTile(
+              title: Text(favEvents[index].planiantEventName),
+              subtitle: Text(favEvents[index].planiantEventDescription),
+              onTap: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) =>
+                            PlaniantEventDetailScreen(planiantEvent: favEvents[index])));
+              },
+            ));
+          },
+        ));
   }
 
   _setProfilePicture(String userId, File profilePicture) async {
